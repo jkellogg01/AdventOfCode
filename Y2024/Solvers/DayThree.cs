520e4254aec7d0de1ab21a7855441b93a0043a79
Joshua Kellogg <135467319+jkellogg01@users.noreply.github.com>
2024-12-03 07:55:35 -0800

    day three cleanup

--- Y2024/Solvers/DayThree.cs
+++ Y2024/Solvers/DayThree.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace Y2024.Solvers;
@@ -31,7 +30,6 @@ public abstract class DayThree
         foreach(var until in disables)
         {
             if (until <= from) continue;
-            Console.WriteLine($"range {from}:{until}");
             sum += SumInstructions(data[from..until]);
             try {
                 from = enables.First(e => e > until);
@@ -40,23 +38,6 @@ public abstract class DayThree
             }
         }
         sum += SumInstructions(data[from..]);
-        // while (true)
-        // {
-        //     var until = disables.Count == 0 ? data.Length : disables.First();
-        //     while (until < from)
-        //     {
-        //         if (disables.Count <= 1) return sum;
-        //         disables.RemoveAt(0);
-        //         until = disables.First();
-        //     }
-        //     // Console.WriteLine($"range {from} - {until}");
-        //     sum += SumInstructions(data[from..until]);
-        //     if (disables.Count == 0) break;
-        //     disables.RemoveAt(0);
-        //     if (enables.Count == 0) break;
-        //     from = enables.First();
-        //     enables.RemoveAt(0);
-        // }
         return sum;
     }
 
