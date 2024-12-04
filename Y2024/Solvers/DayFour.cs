3b59690476a15cd512ee09781dfcb1751a0b677e
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-12-03 21:48:31 -0700

    setup for day four

---
+++ Y2024/Solvers/DayFour.cs
@@ -0,0 +1,16 @@
+namespace Y2024.Solvers;
+
+public abstract class DayFour
+{
+    private static readonly SolutionInput input = new(4);
+
+    public static int PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
