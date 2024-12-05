f73080f9a9dd0af416b2f7d7ce6475283161d18e
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-12-04 21:26:26 -0700

    setup for day 5

---
+++ Y2024/Solvers/DayFive.cs
@@ -0,0 +1,14 @@
+namespace Y2024.Solvers;
+
+public abstract class DayFive
+{
+    public static int PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
