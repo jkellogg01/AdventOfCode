202344202fd5b9131dcd1d5a580324a8fcb700d4
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-12-05 21:55:22 -0700

    day six setup

---
+++ Y2024/Solvers/DaySix.cs
@@ -0,0 +1,16 @@
+namespace Y2024.Solvers;
+
+public abstract class DaySix
+{
+    private static readonly SolutionInput input = new(6);
+
+    public static int PartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int PartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
